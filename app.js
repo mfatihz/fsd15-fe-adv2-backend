f12ca1d4fd1881fcbab54376f99afaf2c7f72e88
mfatihz <fatih@reksolindo.co.id>
2025-08-24 10:10:47 +0700

    temporary include .env in dev phase

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const apiRoute = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: process.env.NODE_ENV === 'production' 
-    ? ['http://localhost:3000', 'http://localhost:5173'] 
-    : '*'
-}));
+// app.use(cors({
+//   origin: process.env.NODE_ENV === 'production' 
+//     ? ['http://localhost:3000', 'http://localhost:5173'] 
+//     : '*'
+// }));
+app.use(cors());
 
 app.use(express.json());
 
